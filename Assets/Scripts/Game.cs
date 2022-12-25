e552128d8574a7f09d29718976480dd93181b21c
SPEEDBOI\Irakusa <qgfifgdrjnoekppzkx@tmmbt.net>
2022-12-25 17:58:06 +0200

    Initial commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+
+
+public class Game : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;   //Игровая валюта
+    private int bonus = 1;
+    private int workersCount, workersBonus = 1;
+    public int[] ShopIdleCosts;
+    public int[] ShopBonusesIdle;
+    public Text[] shopButtonsIdleText;
+    public GameObject ShopIdle;
+    public Button[] ShopButtons;
+    public float[] BonusTime;
+
+    private Save sv = new Save();
+
+
+    public void Start()
+    {
+        StartCoroutine(BonusPerSec()); //Запустить просчёт бонуса в секунду
+    }
+
+   
+
+    public void Update()
+    {
+        scoreText.text = score + "▐";
+    }
+    [Header("Магазин кликов")]
+    public GameObject ShopClick;
+    public int[] ShopCosts;
+    public int[] ShopBonuses;
+    public Text[] shopButtonsText;
+
+
+
+
+
+    public void ShopButtonBonus(int index)
+    {
+        if (score >= ShopCosts[index])
+        {
+            bonus += ShopBonuses[index];
+            score -= ShopCosts[index];
+            ShopCosts[index] *= 2;
+            shopButtonsText[index].text = ShopCosts[index] + "▐";
+        }
+        else
+        {
+            Debug.Log("Не хватает энергии");
+        }
+    }
+
+    public void HireWorker(int index)
+    {
+        if (score >= ShopIdleCosts[index])
+        {
+            workersCount += ShopBonusesIdle[index] ;
+            score -= ShopIdleCosts[index];
+            ShopIdleCosts[index] *= 2;
+            shopButtonsIdleText[index].text = ShopIdleCosts[index] + "▐";
+        }
+    }
+
+   /* public void StartBonusTimer(int index)
+    {
+        int cost = 2 * workersCount;
+        shopButtonsText[2].text = ShopCosts[index] + cost + "▐";
+        if (score > cost)
+        {
+            StartCoroutine(BonusTimer(BonusTime[index], index));
+            score -= cost;
+        }
+    }*/
+
+    IEnumerator BonusPerSec() // Бонус в секунду
+    {
+        while (true) // Бесконечный цикл
+        {
+            
+            score += (workersCount* workersBonus);
+            yield return new WaitForSeconds(1); // Делаем задержку в 1 секунду
+        }
+
+    }
+
+    IEnumerator BonusTimer (float time, int index)
+    {
+        ShopButtons[index].interactable = false;
+        if (index == 0 && workersCount > 0)
+        {
+            workersBonus *= 2;
+            yield return new WaitForSeconds(time);
+            workersBonus /= 2;
+        }
+        ShopButtons[index].interactable = true;
+    }
+
+    public void ShopClicks()
+    {
+        if (ShopIdle.activeSelf)
+        {
+            ShopIdle.SetActive(!ShopIdle.activeSelf);
+        }
+        ShopClick.SetActive(!ShopClick.activeSelf);
+    }
+
+    public void ShopIdles()
+    {
+        if (ShopClick.activeSelf)
+        {
+            ShopClick.SetActive(!ShopClick.activeSelf);
+        }
+        ShopIdle.SetActive(!ShopIdle.activeSelf);
+    }
+
+    public void OnClick()
+    {
+        score += bonus;
+    }
+    [Serializable]
+    public class Save
+    {
+        public int score;
+        public int[] levelOfItem;
+        public int[] bonusCounter;
+    }
+        
+}
---
+++ Assets/Scripts/Game1.cs
@@ -0,0 +1,229 @@
+﻿// Script by Android Helper [Clicker Tutorial] //
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+public class Game1 : MonoBehaviour
+{
+        public GameObject ShopIdle;
+    public GameObject ShopClick;
+    public int allBonusPS = 0;
+    public int allBonPS = 0;
+    [Header("Текст, отвечающий за отображение денег")]
+    public Text scoreText;
+    [Header("Магазин")]
+    public List<Item> shopItems = new List<Item>();
+    [Header("Текст на кнопках товаров")]
+    public Text[] shopItemsText;
+    [Header("Кнопки товаров")]
+    public Button[] shopBttns;
+    [Header("Панелька магазина")]
+    public GameObject shopPan;
+
+    private int score; //Игровая валюта
+    private int scoreIncrease = 1; //Бонус при клике
+    private Save sv = new Save();
+
+
+    private void Awake()
+    {
+        /*if (PlayerPrefs.HasKey("SV"))
+        {
+            int totalBonusPS = 0;
+            sv = JsonUtility.FromJson<Save>(PlayerPrefs.GetString("SV"));
+            score = sv.score;
+            for (int i = 0; i < shopItems.Count; i++)
+            {
+                shopItems[i].levelOfItem = sv.levelOfItem[i];
+                shopItems[i].bonusCounter = sv.bonusCounter[i];
+                if (shopItems[i].needCostMultiplier) shopItems[i].cost *= (int)Mathf.Pow(shopItems[i].costMultiplier, shopItems[i].levelOfItem);
+                if (shopItems[i].bonusIncrease != 0 && shopItems[i].levelOfItem != 0) scoreIncrease += (int)Mathf.Pow(shopItems[i].bonusIncrease, shopItems[i].levelOfItem);
+                totalBonusPS += shopItems[i].bonusPerSec * shopItems[i].bonusCounter;
+            }
+            DateTime dt = new DateTime(sv.date[0], sv.date[1], sv.date[2], sv.date[3], sv.date[4], sv.date[5]);
+            TimeSpan ts = DateTime.Now - dt;
+            int offlineBonus = (int)ts.TotalSeconds * totalBonusPS;
+            score += offlineBonus;
+            print("Вы отсутствовали: " + ts.Days + "Д. " + ts.Hours + "Ч. " + ts.Minutes + "М. " + ts.Seconds + "S.");
+            print("Ваши рабочие заработали: " + offlineBonus + " ϟ");
+        }*/
+    }
+
+    private void Start()
+    {
+        updateCosts(); //Обновить текст с ценами
+        StartCoroutine(BonusPerSec()); //Запустить просчёт бонуса в секунду
+    }
+
+    private void Update()
+    {
+        scoreText.text = score + " ϟ"; //Отображаем деньги
+        allBonusPS = shopItems[8].bonusPerSec * shopItems[8].bonusCounter
+            + shopItems[9].bonusPerSec * shopItems[9].bonusCounter
+            + shopItems[10].bonusPerSec * shopItems[10].bonusCounter
+            + shopItems[11].bonusPerSec * shopItems[11].bonusCounter
+            + shopItems[12].bonusPerSec * shopItems[12].bonusCounter
+            + shopItems[13].bonusPerSec * shopItems[13].bonusCounter
+            + shopItems[14].bonusPerSec * shopItems[14].bonusCounter
+            + shopItems[15].bonusPerSec * shopItems[15].bonusCounter;
+
+
+    }
+
+
+    public void BuyBttn(int index) //Метод при нажатии на кнопку покупки товара (индекс товара)
+    {
+        int cost = shopItems[index].cost * shopItems[shopItems[index].itemIndex].bonusCounter; //Рассчитываем цену в зависимости от кол-ва рабочих (к примеру)
+        if (shopItems[index].itsBonus && score >= cost) //Если товар нажатой кнопки - это бонус, и денег >= цены(е)
+        {
+            if (cost >= 0) // Если цена больше чем 0, то:
+            {
+                StartCoroutine(BonusTimer(shopItems[index].timeOfBonus, index)); //Запускаем бонусный таймер
+            }
+            else print("Нечего улучшать то!"); // Иначе выводим в консоль текст.
+        }
+        else if (score >= shopItems[index].cost) // Иначе, если товар нажатой кнопки - это не бонус, и денег >= цена
+        {
+            if (shopItems[index].itsItemPerSec) shopItems[index].bonusCounter++; // Если нанимаем рабочего (к примеру), то прибавляем кол-во рабочих
+            else scoreIncrease += shopItems[index].bonusIncrease; // Иначе бонусу при клике добавляем бонус товара
+            score -= shopItems[index].cost; // Вычитаем цену из денег
+            if (shopItems[index].needCostMultiplier) shopItems[index].cost *= shopItems[index].costMultiplier; // Если товару нужно умножить цену, то умножаем на множитель
+            shopItems[index].levelOfItem++; // Поднимаем уровень предмета на 1
+        }
+        else print("Не хватает денег!"); // Иначе если 2 проверки равны false, то выводим в консоль текст.
+        updateCosts(); //Обновить текст с ценами
+    }
+
+    private void updateCosts() // Метод для обновления текста с ценами
+    {
+        for (int i = 0; i < shopItems.Count; i++) // Цикл выполняется, пока переменная i < кол-ва товаров
+        {
+            if (shopItems[i].itsBonus) // Если товар является бонусом, то:
+            {
+                int cost = shopItems[i].cost * shopItems[shopItems[i].itemIndex].bonusCounter; // Рассчитываем цену в зависимости от кол-ва рабочих (к примеру)
+                shopItemsText[i].text = shopItems[i].name + "\n" + cost + " ϟ"; // Обновляем текст кнопки с рассчитанной ценой
+            }
+            else shopItemsText[i].text = shopItems[i].name + "\n" + shopItems[i].cost + " ϟ"; // Иначе если товар не является бонусом, то обновляем текст кнопки с обычной ценой
+        }
+    }   
+    public int itemIndex;
+
+    IEnumerator BonusPerSec() // Бонус в секунду
+    {
+        while (true) // Бесконечный цикл
+        {
+            for (int i = 0; i < shopItems.Count; i++) score += (shopItems[i].bonusCounter * shopItems[i].bonusPerSec); // Добавляем к игровой валюте бонус рабочих (к примеру)
+            yield return new WaitForSeconds(1); // Делаем задержку в 1 секунду
+        }
+    }
+   
+    IEnumerator BonusTimer(float time, int index) // Бонусный таймер (длительность бонуса (в сек.),индекс товара)
+    {
+         allBonPS = allBonusPS;
+         shopBttns[index].interactable = false; // Выключаем кликабельность кнопки бонуса
+         shopItems[shopItems[index].itemIndex].bonusPerSec += allBonPS; // Удваиваем бонус рабочих в секунду (к примеру)
+         yield return new WaitForSeconds(time); // Делаем задержку на столько секунд, сколько указали в параметре
+         shopItems[shopItems[index].itemIndex].bonusPerSec -= allBonPS; // Возвращаем бонус в нормальное состояние
+         shopBttns[index].interactable = true; // Включаем кликабельность кнопки бонуса
+        
+    }
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            sv.score = score;
+            sv.levelOfItem = new int[shopItems.Count];
+            sv.bonusCounter = new int[shopItems.Count];
+            for (int i = 0; i < shopItems.Count; i++)
+            {
+                sv.levelOfItem[i] = shopItems[i].levelOfItem;
+                sv.bonusCounter[i] = shopItems[i].bonusCounter;
+            }
+            sv.date[0] = DateTime.Now.Year; sv.date[1] = DateTime.Now.Month; sv.date[2] = DateTime.Now.Day; sv.date[3] = DateTime.Now.Hour; sv.date[4] = DateTime.Now.Minute; sv.date[5] = DateTime.Now.Second;
+            PlayerPrefs.SetString("SV", JsonUtility.ToJson(sv));
+        }
+    }
+#else
+    private void OnApplicationQuit()
+    {
+        sv.score = score;
+        sv.levelOfItem = new int[shopItems.Count];
+        sv.bonusCounter = new int[shopItems.Count];
+        for (int i = 0; i < shopItems.Count; i++)
+        {
+            sv.levelOfItem[i] = shopItems[i].levelOfItem;
+            sv.bonusCounter[i] = shopItems[i].bonusCounter;
+        }
+        sv.date[0] = DateTime.Now.Year; sv.date[1] = DateTime.Now.Month; sv.date[2] = DateTime.Now.Day; sv.date[3] = DateTime.Now.Hour; sv.date[4] = DateTime.Now.Minute; sv.date[5] = DateTime.Now.Second;
+        PlayerPrefs.SetString("SV", JsonUtility.ToJson(sv));
+    }
+#endif
+
+    public void showShopPan() // Рассказывал в уроке #2
+    {
+        shopPan.SetActive(!shopPan.activeSelf); // Рассказывал в уроке #2
+    }
+
+    public void OnClick() // Рассказывал в уроке #1
+    {
+        score += scoreIncrease; // К игровой валюте прибавляем бонус при клике
+    }
+
+    public void Restart()
+    {
+        score = 0;
+        scoreIncrease = 1;
+        for (int i = 0; i < shopItems.Count; i++)
+        {
+            sv.levelOfItem[i] = 0;
+            sv.bonusCounter[i] = 0;
+
+        }
+    }
+}
+[Serializable]
+public class Item // Класс товара
+{
+    [Tooltip("Название используется на кнопках")]
+    public string name;
+    [Tooltip("Цена товара")]
+    public int cost;
+    [Tooltip("Бонус, который добавляется к бонусу при клике")]
+    public int bonusIncrease;
+    [HideInInspector]
+    public int levelOfItem; // Уровень товара
+    [Space]
+    [Tooltip("Нужен ли множитель для цены?")]
+    public bool needCostMultiplier;
+    [Tooltip("Множитель для цены")]
+    public int costMultiplier;
+    [Space]
+    [Tooltip("Этот товар даёт бонус в секунду?")]
+    public bool itsItemPerSec;
+    [Tooltip("Бонус, который даётся в секунду")]
+    public int bonusPerSec;
+    [HideInInspector]
+    public int bonusCounter; // Кол-во рабочих (к примеру)
+    [Space]
+    [Tooltip("Это временный бонус?")]
+    public bool itsBonus;
+    [Tooltip("Множитель товара, который управляется бонусом (Умножается переменная bonusPerSec)")]
+    public int itemMultiplier;
+    [Tooltip("Индекс товара, который будет управляться бонусом (Умножается переменная bonusPerSec этого товара)")]
+    public int itemIndex = 8;
+    [Tooltip("Длительность бонуса")]
+    public float timeOfBonus;
+
+}
+[Serializable]
+public class Save
+{
+    public int score;
+    public int[] levelOfItem;
+    public int[] bonusCounter;
+    public int[] date = new int[6];
+}
